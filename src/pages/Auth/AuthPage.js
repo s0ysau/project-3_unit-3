706b7d090bc517e5a56107587e047287e874eadc
jerricklc@gmail.com <jerricklc@gmail.com>
2023-01-11 17:38:34 -0500

    styled the auth page and added something to README

--- src/pages/Auth/AuthPage.js
+++ src/pages/Auth/AuthPage.js
@@ -4,9 +4,17 @@ import LoginForm from '../../components/LoginForm/LoginForm'
 export default function AuthPage (props) {
   return (
     <main>
-      <h1>Authentication Page</h1>
-      <SignUpForm setUser={props.setUser} />
-      <LoginForm setUser={props.setUser} />
+      <h1 className='title'>MicroParts</h1>
+      <div className='welcome-container'>
+        <div className='sign-up-section'>
+          <p className='signup-title'>Sign Up</p>
+          <SignUpForm setUser={props.setUser} />
+        </div>
+        <div className='login-section'>
+          <p className='login-title'>Already have an Account?</p>
+          <LoginForm setUser={props.setUser} />
+        </div>
+      </div>
     </main>
   )
 }
